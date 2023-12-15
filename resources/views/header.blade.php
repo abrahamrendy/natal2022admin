eccba41954aaacade305bda72ceb0070072da5cb
abrahamrendy <aeroponse@gmail.com>
2023-12-15 22:33:43 +0700

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>GBI Sukawarna Christmas 2022 Registration</title>
+    <title>GBI Sukawarna Christmas <?php echo date('Y', strtotime('now'))?> Registration</title>
 
     <!-- Scripts -->
     <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
