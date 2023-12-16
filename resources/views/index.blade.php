71a609fb37fdad1201b4d76a89db1dc3110883ac
abrahamrendy <aeroponse@gmail.com>
2023-12-16 09:30:43 +0700

    test commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-        @include('header')
+    @include('header')
             
     <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
         <!-- begin:: Page -->
